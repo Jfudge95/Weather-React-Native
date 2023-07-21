325fd6f441c9dc80e5d318bd704049d233ce0c71
Jordan Fudge <jordanfudge95@gmail.com>
2023-07-21 18:56:29 -0400

    Filled MeteoAdvanced with data

--- components/MeteoAdvanced/MeteoAdvanced.jsx
+++ components/MeteoAdvanced/MeteoAdvanced.jsx
@@ -7,18 +7,19 @@ import {
 } from "./MeteoAdvanced.style.js";
 import { Txt } from "../Txt/Txt.jsx";
 export function MeteoAdvanced({ sunrise, sunset, windspeed }) {
+  const windspeedMph = Math.round(windspeed * 0.621371);
   return (
     <View style={s.container}>
       <StyledContainer>
-        <StyledLabel>PlaceHolder{sunrise}</StyledLabel>
+        <StyledLabel>{sunrise}</StyledLabel>
         <StyledValue>Sunrise</StyledValue>
       </StyledContainer>
       <StyledContainer>
-        <StyledLabel>PlaceHolder{sunset}</StyledLabel>
+        <StyledLabel>{sunset}</StyledLabel>
         <StyledValue>Sunset</StyledValue>
       </StyledContainer>
       <StyledContainer>
-        <StyledLabel>PlaceHolder{windspeed}</StyledLabel>
+        <StyledLabel>{windspeedMph} mp/h</StyledLabel>
         <StyledValue>Windspeed</StyledValue>
       </StyledContainer>
     </View>
