32839464e59f69b6d2abd8deafd0bd03b5ad76c9
Jordan Fudge <jordanfudge95@gmail.com>
2023-07-21 18:37:10 -0400

    Fetched City Data using reverse geocoding

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { s } from "./App.style";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 import { Home } from "./pages/Home/Home";
 import { ImageBackground } from "react-native";
 import backgroundImg from "./assets/background.png";
@@ -14,6 +18,7 @@ import { useFonts } from "expo-font";
 export default function App() {
   const [coordinates, setCoordinates] = useState();
   const [weather, setWeather] = useState();
+  const [city, setCity] = useState();
   const [isFontLoaded] = useFonts({
     "Alata-Regular": require("./assets/fonts/Alata-Regular.ttf"),
   });
@@ -26,12 +31,18 @@ export default function App() {
   useEffect(() => {
     if (coordinates) {
       fetchWeatherByCoords(coordinates);
+      fetchCityByCoords(coordinates);
     }
   }, [coordinates]);
 
   async function fetchWeatherByCoords(coords) {
     const weatherResponse = await MeteoAPI.fetchWeatherByCoords(coords);
     setWeather(weatherResponse);
+  }
+
+  async function fetchCityByCoords(coords) {
+    const cityResponse = await MeteoAPI.fetchCityByCoords(coords);
+    setCity(cityResponse);
   }
 
   async function getUserCoordinates() {
@@ -55,7 +66,7 @@ export default function App() {
     >
       <SafeAreaProvider>
         <SafeAreaView style={s.container}>
-          {isFontLoaded && weather && <Home weather={weather} />}
+          {isFontLoaded && weather && <Home city={city} weather={weather} />}
         </SafeAreaView>
       </SafeAreaProvider>
     </ImageBackground>
